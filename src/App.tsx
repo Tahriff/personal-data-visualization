ef905ff2707adac4d65da090bc54d126bc11ee18
Tahriff <alexandre.derville@matera.eu>
2023-01-18 22:11:07 +0100

    setup eslint prettier

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -27,6 +27,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>gpo </div>
     </div>
   )
 }
